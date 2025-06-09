5b082369956d635f8a79799f0d0a27c12ab3783f
Adam Hart <78327814+AdamHHart@users.noreply.github.com>
2025-06-09 10:05:36 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
         <Route path="/auth/reset-password" element={<ResetPasswordPage />} />
         <Route path="/email-test" element={<EmailTestPage />} />
         <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route index element={<Navigate to="/dashboard\" replace />} />
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="onboarding" element={<OnboardingPage />} />
           <Route path="checklist" element={<ChecklistPage />} />
